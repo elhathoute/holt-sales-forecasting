41cb6a53cd12ca9a687162f3ed44dd3572cffb68
elhathoutWobz <abdelaziz.e@decq.com>
2025-05-24 14:14:19 +0100

    add comnd

---
+++ run_app.py
@@ -0,0 +1,2 @@
+import os
+os.system("/Users/abdelazizelhathout/Documents/Freelance/holt-sales-forecasting/venv/bin/streamlit run /Users/abdelazizelhathout/Documents/Freelance/holt-sales-forecasting/app.py")
